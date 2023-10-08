260b080a83164111d2211e5cfd93267bed292411
Dhananjay <dkgurav0101@gmail.com>
2023-10-08 21:20:59 +0530

    creating setup.py for packaging and publishing project

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages,setup
+from typing import List
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+HYPEN_E_DOT = '-e .'
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mentioned in requirements.txt file 
+    """
+    requirements=[]
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirements = requirement_file.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+                requirements.remove(HYPEN_E_DOT)
+
+
+    return requirements
+
+
+setup (
+    name ='sensor',
+    version='0.0.1',
+    author='dhananjay gurav',
+    author_email='dkgurav0101@gmail.com',
+    packages= find_packages(),
+    install_requires=get_requirements_list()
+)
+
+
+# if __name__ == "__main__":
+#     requirements_list = get_requirements_list()
