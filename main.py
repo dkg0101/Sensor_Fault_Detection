a997497ea4adbc5d3fe7a11cf1c2600b3923b3a3
Dhananjay <dkgurav0101@gmail.com>
2023-11-11 13:24:57 +0530

    Logging and exception added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.logger import logging
 
 
+logging.info("this is first log")
+print('successs')
 
-
-if __name__=='__main__':
-    mongodb_client = MongoDBClient()
-    print("collection name: ",mongodb_client.database.list_collection_names())
-    # mongodb_client.client.close()
+# if __name__=='__main__':
+#     mongodb_client = MongoDBClient()
+#     print("collection name: ",mongodb_client.database.list_collection_names())
+#     # mongodb_client.client.close()
