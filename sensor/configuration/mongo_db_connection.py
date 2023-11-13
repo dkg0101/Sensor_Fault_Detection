040cc8ae25456813a648275dc679b531d5482609
Dhananjay <dkgurav0101@gmail.com>
2023-11-13 11:25:38 +0530

    Mongodb connection and creating dataframe

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,8 +1,10 @@
+from sensor.logger import logging
 import pymongo
+from sensor.exception import SensorException
 from sensor.constant.database import DATABASE_NAME
-# from sensor.constant.env_variable import MONGODB_URL_KEY
+from sensor.constant.env_variable import MONGODB_URL_KEY
 import certifi
-import os
+import os,sys
 ca = certifi.where()
 
 class MongoDBClient:
@@ -11,11 +13,23 @@ class MongoDBClient:
     def __init__(self,database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url ="<your mongodb connection url"
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                logging.info("Connecting  to MongoDBClient... ")
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tlsCAFile=ca)
             self.client =MongoDBClient.client
+            print(self.client)
             self.database = self.client[database_name]
+            print(self.database)
             self.database_name = database_name
+            print(self.database_name)
+            logging.info(f"Connected database: '{self.database_name}'")
 
         except Exception as e:
-            raise e
+            raise SensorException(e,sys)
+
+if __name__=="__main__":
+    c = MongoDBClient()
+    # print(c.database_name)
+
+
+
