e39e8b827a3fc1f7ad94434a1b10f8dffdc15b35
dkg0101 <dkgurav0101@gmail.com>
2023-12-02 11:26:07 +0530

    main.py updated

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -11,7 +11,10 @@ from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
 from sensor.components.model_evaluation import ModelEvaluation
 from sensor.components.model_pusher import ModelPusher
+
 class TrainPipeline:
+
+    is_pipeline_running=False
 
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
@@ -93,8 +96,12 @@ class TrainPipeline:
         except  Exception as e:
             raise  SensorException(e,sys)
         
+
+
     def run_pipeline(self):
         try:
+            TrainPipeline.is_pipeline_running=True
+
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
@@ -104,5 +111,8 @@ class TrainPipeline:
                 raise Exception("Trained model is not better than the best model")
             model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
 
+            TrainPipeline.is_pipeline_running=False
+
         except  Exception as e:
+            TrainPipeline.is_pipeline_running=False
             raise  SensorException(e,sys)
